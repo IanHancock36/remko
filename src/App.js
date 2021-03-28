0df87c694be6e617fa10f77c381c62b6271bf2db
Ian Hancock <northroadian@gmail.com>
2021-03-28 15:48:17 -0500

    ga done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 
 import './App.css';
 import Questions from './Components/Questions'
+import React, {useEffect}from "react";
+import ReactGA from "react-ga";
+
 function App() {
+
+  useEffect(() => {
+    ReactGA.initialize('267091666');
+    ReactGA.pageview(window.location.pathname);
+  })
+
+
   return (
     <div className="app">
      <Questions />
--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ReactGA from 'react-ga';
-ReactGA.initialize('2067091666'); // add your tracking id here.
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
